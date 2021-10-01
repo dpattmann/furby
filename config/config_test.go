8757a97b9319e8c7b70d7362246efdf3fe2b90df
Vladislav Kravitski <vladislav.kravitski@otto.de>
2021-10-01 19:56:57 +0200

    fix and adapt tests for config

--- config/config_test.go
+++ config/config_test.go
@@ -1,78 +1,167 @@
 package config
 
 import (
+	"log"
 	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestNewConfig(t *testing.T) {
+const (
+	validConfig = `
+		{    
+			"client_credentials" : {
+        		"id": "TestClientId",
+        		"scopes": [
+        		    "scopeA",
+        		    "scopeB"
+        		],
+        		"secret": "TestClientSecret",
+        		"url": "https://localhost"
+			},
+    		"server": {
+    		    "cert": "foo.cert",
+    		    "key": "foo.key",
+    		    "tls": "true"
+    		}
+		}
+	`
+	configWithInvalidUrl = `
+		{    
+			"client_credentials" : {
+        		"id": "TestClientId",
+        		"scopes": [
+        		    "scopeA",
+        		    "scopeB"
+        		],
+        		"secret": "TestClientSecret",
+        		"url": "localhost"
+			}
+		}
+	`
+	configWithDisabledTls = `
+		{    
+			"client_credentials" : {
+        		"id": "TestClientId",
+        		"scopes": [
+        		    "scopeA",
+        		    "scopeB"
+        		],
+        		"secret": "TestClientSecret",
+        		"url": "https://localhost"
+			},
+    		"server": {
+    		    "cert": "",
+    		    "key": "",
+    		    "tls": "false"
+    		}
+		}
+	`
+
+	configWithTlsButWithoutCert = `
+		{    
+			"client_credentials" : {
+        		"id": "TestClientId",
+        		"scopes": [
+        		    "scopeA",
+        		    "scopeB"
+        		],
+        		"secret": "TestClientSecret",
+        		"url": "https://localhost"
+			},
+    		"server": {
+    		    "cert": "",
+    		    "key": "",
+    		    "tls": "true"
+    		}
+		}
+	`
+)
+
+func createTempConfig(content string) {
+	f, err := os.Create("test_temp.json")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer f.Close()
+
+	_, err2 := f.WriteString(content)
+
+	if err2 != nil {
+		log.Fatal(err2)
+	}
+
+}
+
+func removeTempConfig() {
+	err := os.Remove("test_temp.json")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func TestNewValidConfig(t *testing.T) {
 	t.Run("Create valid config from environment", func(t *testing.T) {
-		_ = os.Setenv("FURBY_CLIENTCREDENTIALS_ID", "TestClientId")
-		_ = os.Setenv("FURBY_CLIENTCREDENTIALS_SECRET", "TestClientSecret")
-		_ = os.Setenv("FURBY_CLIENTCREDENTIALS_URL", "https://localhost")
-		_ = os.Setenv("FURBY_CLIENTCREDENTIALS_SCOPES", "scopeA scopeB")
+		createTempConfig(validConfig)
 
-		got, err := NewConfig()
+		got, err := NewConfig("./test_temp.json")
 
-		want := &Config{ClientCredentials: ClientCredentials{
-			Id:     "TestClientId",
-			Scopes: []string{"scopeA", "scopeB"},
-			Secret: "TestClientSecret",
-			Url:    "https://localhost",
-		}}
+		want := &Config{
+			ClientCredentials: ClientCredentials{
+				Id:     "TestClientId",
+				Scopes: []string{"scopeA", "scopeB"},
+				Secret: "TestClientSecret",
+				Url:    "https://localhost",
+			},
+			Server: Server{
+				Cert: "foo.cert",
+				Key:  "foo.key",
+				Tls:  true,
+			},
+		}
 
 		assert.NoError(t, err)
 		assert.Equal(t, want, got)
+
+		removeTempConfig()
 	})
+}
 
-	t.Run("Create config with invalid url", func(t *testing.T) {
-		_ = os.Setenv("FURBY_CLIENTCREDENTIALS_ID", "TestClientId")
-		_ = os.Setenv("FURBY_CLIENTCREDENTIALS_SECRET", "TestClientSecret")
-		_ = os.Setenv("FURBY_CLIENTCREDENTIALS_URL", "localhost")
-		_ = os.Setenv("FURBY_CLIENTCREDENTIALS_SCOPES", "scopeA scopeB")
+func TestNewFailureConfig(t *testing.T) {
+	tests := []struct {
+		testCase        string
+		testDescription string
+		wantErr         bool
+	}{
+		{
+			testCase:        configWithInvalidUrl,
+			testDescription: "Create config with invalid url",
+			wantErr:         true,
+		},
+		{
+			testCase:        configWithDisabledTls,
+			testDescription: "Create config without tls config",
+			wantErr:         false,
+		},
+		{
+			testCase:        configWithTlsButWithoutCert,
+			testDescription: "Create config with tls but without cert files",
+			wantErr:         true,
+		},
+	}
 
-		_, err := NewConfig()
+	for _, tt := range tests {
+		t.Run(tt.testDescription, func(t *testing.T) {
+			createTempConfig(tt.testCase)
+			if _, err := NewConfig("./test_temp.json"); (err != nil) != tt.wantErr {
+				t.Errorf("Expected %v, wantErr %v", err, tt.wantErr)
+			}
+			removeTempConfig()
+		})
+	}
 
-		assert.Error(t, err)
-	})
-
-	t.Run("Create config without tls config", func(t *testing.T) {
-		_ = os.Setenv("FURBY_CLIENTCREDENTIALS_ID", "TestClientId")
-		_ = os.Setenv("FURBY_CLIENTCREDENTIALS_SECRET", "TestClientSecret")
-		_ = os.Setenv("FURBY_CLIENTCREDENTIALS_URL", "https://localhost")
-		_ = os.Setenv("FURBY_CLIENTCREDENTIALS_SCOPES", "scopeA scopeB")
-		_ = os.Setenv("FURBY_SERVER_TLS", "false")
-
-		_, err := NewConfig()
-
-		assert.NoError(t, err)
-	})
-
-	t.Run("Create config without tls config but without cert files", func(t *testing.T) {
-		_ = os.Setenv("FURBY_CLIENTCREDENTIALS_ID", "TestClientId")
-		_ = os.Setenv("FURBY_CLIENTCREDENTIALS_SECRET", "TestClientSecret")
-		_ = os.Setenv("FURBY_CLIENTCREDENTIALS_URL", "https://localhost")
-		_ = os.Setenv("FURBY_CLIENTCREDENTIALS_SCOPES", "scopeA scopeB")
-		_ = os.Setenv("FURBY_SERVER_TLS", "true")
-
-		_, err := NewConfig()
-
-		assert.Error(t, err)
-	})
-
-	t.Run("Create config without tls config but without cert files", func(t *testing.T) {
-		_ = os.Setenv("FURBY_CLIENTCREDENTIALS_ID", "TestClientId")
-		_ = os.Setenv("FURBY_CLIENTCREDENTIALS_SECRET", "TestClientSecret")
-		_ = os.Setenv("FURBY_CLIENTCREDENTIALS_URL", "https://localhost")
-		_ = os.Setenv("FURBY_CLIENTCREDENTIALS_SCOPES", "scopeA scopeB")
-		_ = os.Setenv("FURBY_SERVER_TLS", "true")
-		_ = os.Setenv("FURBY_SERVER_CERT", "foo.cert")
-		_ = os.Setenv("FURBY_SERVER_KEY", "foo.key")
-
-		_, err := NewConfig()
-
-		assert.NoError(t, err)
-	})
 }
